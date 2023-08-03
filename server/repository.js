73468a2d1a6bc482709b1eca3e9e43bf3861b3de
Gene Gil Sario <gene.sario@developersinstitute.co.nz>
2023-08-03 17:03:34 +1200

    feat: implement the repository design pattern

--- server/repository.js
+++ server/repository.js
@@ -1,5 +1,30 @@
 const db = require("./db");
 
 module.exports = {
-  getGradeScale: async () => {},
+  getGradeScale: async () => {
+    try {
+      const query = `
+      SELECT
+        level,
+        min_mark,
+        max_mark,
+        grade
+      FROM grade_scale 
+      ORDER BY
+        id
+    `;
+
+      const result = await db.query(query);
+
+      const grades = result.rows.map((row) => ({
+        level: row.level,
+        min_mark: row.min_mark,
+        max_mark: row.max_mark,
+        grade: row.grade,
+      }));
+      return grades;
+    } catch (error) {
+      throw Error("Failed to fetch grade scale data: " + error.message);
+    }
+  },
 };
