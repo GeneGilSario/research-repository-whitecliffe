3ff823137e3f0affe4dc6dcdee6c27a314627baa
Gene Gil Sario <gene.sario@developersinstitute.co.nz>
2023-08-09 13:29:39 +1200

    fix: simplifying the return process

--- server/repository.js
+++ server/repository.js
@@ -16,13 +16,12 @@ module.exports = {
 
       const result = await db.query(query);
 
-      const grades = result.rows.map((row) => ({
+      return result.rows.map((row) => ({
         level: row.level,
         min_mark: row.min_mark,
         max_mark: row.max_mark,
         grade: row.grade,
       }));
-      return grades;
     } catch (error) {
       throw Error("Failed to fetch grade scale data: " + error.message);
     }
